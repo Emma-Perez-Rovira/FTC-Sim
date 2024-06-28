5ce79973bc95f8b06b93b80959a156d4ead6de89
Emma <70605739+Emma-Perez-Rovira@users.noreply.github.com>
2024-06-28 09:22:12 -0400

    Outtake now integrated
    Now the player can control the outtake (in and out) with gamepad axis inputs.

--- Slides.cs
+++ Slides.cs
@@ -22,9 +22,13 @@ public class Slides : MonoBehaviour
     private float minExtension = 1.0f;
     private Boolean Extended = false;
     private int multiplyer = 1;
+    private float outtakeOutFloat;
+    private float outtakeInFloat;
     // Start is called before the first frame update
     public void OnSlidesUp(InputAction.CallbackContext ctx) => slidesButton = ctx.action.triggered;
     public void OnSlidesDown(InputAction.CallbackContext ctx) => slidesDownButton = ctx.action.triggered;
+    public void OnOuttakeOut(InputAction.CallbackContext ctx) => outtakeOutFloat = ctx.ReadValue<float>();
+    public void OnOuttakeIn(InputAction.CallbackContext ctx) => outtakeInFloat = -ctx.ReadValue<float>();
     private void Awake()
     {
     }
@@ -54,7 +58,7 @@ public class Slides : MonoBehaviour
                     if (slideSegments[i].transform.localPosition.z < maxExtension * multiplyer)
                         slideSegments[i].transform.localPosition = new Vector3(slideSegments[i].transform.localPosition.x, slideSegments[i].transform.localPosition.y, slideSegments[i].transform.localPosition.z + extensionSpeed);
                 }
-                outtake.transform.Rotate(50 * (slideSegments[0].transform.localPosition.z / maxExtension), 0, 0);
+
             }
             
         }
@@ -67,6 +71,26 @@ public class Slides : MonoBehaviour
                     if (slideSegments[i].transform.localPosition.z > minExtension)
                         slideSegments[i].transform.localPosition = new Vector3(slideSegments[i].transform.localPosition.x, slideSegments[i].transform.localPosition.y, slideSegments[i].transform.localPosition.z - extensionSpeed);
             }
+
         }
+        if(Mathf.Abs(outtakeInFloat) > Mathf.Abs(outtakeOutFloat))
+        {
+            if (outtake.transform.localRotation.z > .50f)
+            {
+                outtake.transform.Rotate(5f * outtakeInFloat, 0, 0);
+            }
+        } else
+        {
+            if (outtake.transform.localRotation.z < .96f)
+            {
+                outtake.transform.Rotate(5f * outtakeOutFloat, 0, 0);
+            }
+        }
+        Debug.Log(outtakeInFloat + ", " + outtakeOutFloat + ",, " + outtake.transform.localRotation.z);
+    }
+    public static float degreesToRad(float degrees)
+    {
+        float radians = (float)(Math.PI / 180) * degrees;
+        return (radians);
     }
 }
